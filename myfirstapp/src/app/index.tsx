91569472fc1ee099315b83aeb0d6b5e8ac5ef60a
BATENDER <vinicius.torres.san@gmail.com>
2025-03-24 18:52:27 -0300

    saving some modifications

--- myfirstapp/src/app/index.tsx
+++ myfirstapp/src/app/index.tsx
@@ -1,9 +1,12 @@
-import { View, Text } from "react-native"
+import { View, Text } from "react-native";
+import '../../global.css';
 
 export default function Index() {
     return (
-        <View>
-            <Text> Hello World! </Text>
+        <View style={{ $$css: true, _: 'bg-slate-100 rounded-xl' }}>
+            <Text style={{ $$css: true, _: 'text-8xl font-medium' }}> Hello World! </Text>
+            <Text style={{ $$css: true, _: 'text-2xl antialised' }}> Hello World! </Text>
+            <Text style={{ $$css: true, _: 'text-left' }}> Hello World! </Text>
         </View>
     )
 }
